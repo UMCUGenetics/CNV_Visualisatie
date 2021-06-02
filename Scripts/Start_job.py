d336234f70aaea7d73245d31b73b518ff48cb5b7
AloysKuster1 <aa.kuster@student.han.nl>
2021-06-02 09:24:04 +0200

    Improved memory usage

--- Scripts/Start_job.py
+++ Scripts/Start_job.py
@@ -56,16 +56,17 @@ def merge_bedfiles(chromosomes, extension):
     :param chromosomes: Int or Str specifying the chromosome.
     :param extension: Str representing the file extension.
     """
-    bedfile_text = 'track name=Flags description="Flags regions of interest." db=hg19 gffTags=on itemRGB="On"\n'
+    with open(f"{args.output}/{args.name}.{extension}", 'w') as output:
+        output.write('track name=Flags description="Flags regions of interest." db=hg19 gffTags=on itemRGB="On"\n')
 
     for chromosome in chromosomes:
-        bedfile_text = update_text(bedfile_text, chromosome, extension)
+        bedfile_text = get_bed_text(chromosome, extension)
 
-    with open(f"{args.output}/{args.name}.{extension}", 'w') as output:
-        output.write(bedfile_text)
+        with open(f"{args.output}/{args.name}.{extension}", 'a') as output:
+            output.write(bedfile_text)
 
 
-def update_text(bedfile_text, chromosome, extension):
+def get_bed_text(chromosome, extension):
     """ The update text function receives the already merged bedfile_text and adds the content of the next bedfile to
     the total bedfile_text.
 
@@ -73,6 +74,7 @@ def update_text(bedfile_text, chromosome, extension):
     :param chromosome: a string resembling a chromosome.
     :param extension: a string resembling the file extension.
     """
+    bedfile_text = str
     try:
         with open(f"{args.output}/{args.name}_{chromosome}.{extension}", 'r') as bedfile:
             text = bedfile.readlines()[1:]
@@ -87,35 +89,30 @@ def update_text(bedfile_text, chromosome, extension):
     return bedfile_text
 
 
-def bedfile_handle(chromosomes):
+def bedfile_handle(chromosomes, extension):
     """ The bedfile_handle function divides the chromosomes over the number of cores to multiprocess the Flag_placer.py
     script.
 
     :param chromosomes: A list of all chromosomes.
     """
-    with Pool(args.cores) as p:
-        p.map(write_bedfile, chromosomes)
+    if extension == 'flags':
+        with Pool(args.cores) as p:
+            p.map(write_bedfile, chromosomes)
 
-
-def bedgraph_handle(chromosomes):
-    """ The bedgraph_handle function divides the chromosomes over the number of cores to multiprocess the
-    softclip_graph.py script.
-
-    :param chromosomes: A list of all chromosomes.
-    """
-    with Pool(args.cores) as p:
-        p.map(write_bedgraphfile, chromosomes)
+    else:
+        with Pool(args.cores) as p:
+            p.map(write_bedgraphfile, chromosomes)
 
 
 if __name__ == '__main__':
     start = time.time()  # Keep track of time.
     chromosomes = list(range(1, 23)) + ['X', 'Y']  # create a list of all chromosomes.
 
-    bedfile_handle(chromosomes)  # create bed files for each chromosome.
+    bedfile_handle(chromosomes, 'flags')  # create bed files for each chromosome.
 
     merge_bedfiles(chromosomes, 'bed')  # merge bed files created by bedfile_handle.
 
-    bedgraph_handle(chromosomes)  # create BedGraph files for each chromosome.
+    bedfile_handle(chromosomes, 'softclips')  # create BedGraph files for each chromosome.
 
     merge_bedfiles(chromosomes, 'BedGraph')  # merge BedGraph files created by bedgraph_handle.
 
