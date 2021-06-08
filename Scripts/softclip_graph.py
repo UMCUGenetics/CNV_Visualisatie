2019d0fd7d556f1ef384b1bbad038041e8405923
AloysKuster1 <aa.kuster@student.han.nl>
2021-06-08 15:17:19 +0200

    Execution time reduced

--- Scripts/softclip_graph.py
+++ Scripts/softclip_graph.py
@@ -4,7 +4,6 @@ import argparse
 import os
 import re
 
-
 parser = argparse.ArgumentParser()
 parser.add_argument('--bam', '-b', required=True, type=str, help='Path to bam file.')
 parser.add_argument('--output', '-o', required=False, type=str, help='Path to output folder.')
@@ -20,12 +19,15 @@ args = parser.parse_args()
 def fetch_reads():
     """ The fetch_reads function fetches the reads from the bam file
 
-    :return reads: Pysam object containing read information
+    :return reads: Pysam object containing read information.
+    :return pileup: Pysam object containing read information of all positions in the region.
     """
+    global bamfile
     bamfile = pysam.AlignmentFile(args.bam, 'rb')
 
     if args.region == 'all':
         reads = bamfile.fetch()
+        pileup = bamfile.pileup()
 
     else:
         if ':' in args.region and '-' in args.region:
@@ -33,12 +35,14 @@ def fetch_reads():
             chromosome = chromosome.replace('chr', '')
 
             reads = bamfile.fetch(chromosome, int(start), int(end))
+            pileup = bamfile.pileup(chromosome, int(start), int(end))
 
         else:
             chromosome = args.region.replace('chr', '')
             reads = bamfile.fetch(chromosome)
+            pileup = bamfile.pileup(chromosome)
 
-    return reads
+    return reads, pileup
 
 
 def get_softclipdata(reads):
@@ -47,22 +51,16 @@ def get_softclipdata(reads):
 
     :param reads: A pysam object containing all the reads with their information.
     :return softclipdata: A dictionary with positions and the number of "normal" bases and softclip bases.
-    :read_data: a list containing the total number of reads and the reads it could not interpret.
+    :return read_data: a list containing the total number of reads and the reads it could not interpret.
     """
     softclipdata = {}
-    handle = {}
     read_data = [0, 0, 0]
 
     for read in reads:
         if not read.is_unmapped and len(read.positions) != 0:
-            read_start = true_start(read.cigar, read.positions[0])
-            regions = softclip_regions(read_start, read.cigar)
-
-            for region in regions:
-                handle = update_softclipdata(region, handle)
-
-            new_softclipdata, handle = remove_old(handle, read_start)
-            softclipdata = {**softclipdata, **new_softclipdata}
+            if 'S' in read.cigarstring:
+                softclip_pos = get_softclip_positions(read)
+                softclipdata = update_softclipdata(softclipdata, softclip_pos)
 
         elif read.is_unmapped:
             read_data[1] += 1
@@ -70,78 +68,25 @@ def get_softclipdata(reads):
             read_data[2] += 1
         read_data[0] += 1
 
-    softclipdata = {**softclipdata, **handle}
-
     return softclipdata, read_data
 
 
-def remove_old(handle, read_start):
-    """ The remove old function removes the basepairs that do not have sofclips to save memory.
+def get_softclip_positions(read):
+    """ The get_softclip_positions function receives a read and returns a list with the positions of softclipped bases.
 
-    :param softclip_data: A dictionary with positions and the number of "normal" bases and softclip bases.
-    :param read_start: An integer representing the place where the read start.
-    :return sofclip_data: A dictionary with positions and the number of "normal" bases and softclip bases.
+    :param read: Pysam object containing read information.
+    :return softclip_pos: A list containing the positions of softclipped bases.
     """
-    remove = []
-    new_softclip_data = {}
-    for position in handle:
-        if position < (read_start-151):
-            if handle[position][1] == 0:
-                remove.append(position)
-            else:
-                new_softclip_data.update({position: handle[position]})
-                remove.append(position)
+    softclip_pos = []
 
-        else:
-            break
-
-    for position in remove:
-        handle.pop(position, None)
-
-    return new_softclip_data, handle
-
-
-def update_softclipdata(region, softclipdata):
-    """ The update_softclipdata function updates the softclipdata dictionary with new information.
-
-    :param region: A region of a read defined by the cigar string.
-    :param softclipdata: A dictionary with positions and the number of "normal" bases and softclip bases.
-    :return sofclipdata: A dictionary with positions and the number of "normal" bases and softclip bases.
-    """
-    if region[2] == 'normal':
-        index = 0
-    else:
-        index = 1
-
-    for pos in range(region[0], region[1]):
-        if pos in softclipdata:
-            softclipdata[pos][index] += 1
-        else:
-            posdata = [0, 0]
-            posdata[index] += 1
-            softclipdata.update({pos: posdata})
-
-    return softclipdata
-
-
-def softclip_regions(read_start, cigar):
-    """ The softclip regions function iterates over the cigar string and returns the positions and if they are "normal"
-    or a softclipped region.
-
-    :param read_start: An integer representing the start of the read (including unmapped bases)
-    :param cigar: a list containing tuples representing the cigar string.
-    :return regions: a 2d list containing the regions of a read and specifying if they are "normal" or softclipped.
-    """
-    regions = []
-    cursor = read_start
-    for element in cigar:
+    cursor = true_start(read.cigar, read.positions[0])
+    for element in read.cigartuples:
         if element[0] == 4:
-            regions.append([cursor, cursor+element[1], 'softclip'])
-        else:
-            regions.append([cursor, cursor+element[1], 'normal'])
+            softclip_pos += [x for x in range(cursor, cursor + element[1])]
+
         cursor += element[1]
 
-    return regions
+    return softclip_pos
 
 
 def true_start(cigar, matchstart):
@@ -150,6 +95,7 @@ def true_start(cigar, matchstart):
 
     :param cigar: a list containing tuples representing the cigar string.
     :param matchstart: an integer representing the start of the first mapped base of a read.
+    :return read_start: an integer representing the starting position of the read including softclips.
     """
     overshoot = 0
 
@@ -164,53 +110,80 @@ def true_start(cigar, matchstart):
     return read_start
 
 
-def get_heatmapdata(sofclipdata):
-    """ The heatmapdata function receives the softclipdata and translates it to a more suitable format for a bedfile.
+def update_softclipdata(softclipdata, softclip_pos):
+    """ The update_softclipdata function updates the softclipdata dictionary when a new softclipped base is found.
 
-    :param sofclipdata: A dictionary with positions and the number of "normal" bases and softclip bases.
-    :return heatmapdata: A 2d list containing the coordinates and the percentage of sofclipped bases.
+    :param softclipdata: A dictionary with positions and the number of softclip bases.
+    :param softclip_pos: An integer representing the position of the softclip base on the reference genome.
+    :return softclipdata: A dictionary with positions and the number of softclip bases.
     """
-    heatmapdata = []
-    first = True
+    for pos in softclip_pos:
+        if pos in softclipdata:
+            softclipdata[pos][0] += 1
+        else:
+            softclipdata.update({pos: [1, 0]})
+
+    return softclipdata
+
+
+def get_coverage(ref, pos):
+    """ The get_coverage function retrieves the number of aligned reads of a position.
+
+    :param ref: a string representing the chromosome.
+    :param pos: an integer representing the position of the reference genome.
+    :return coverage: an integer representing the coverage of the given position.
+    """
+    bamfile = pysam.AlignmentFile(args.bam, 'rb')
+    coverage = 0
+    for pileupcolumn in bamfile.pileup(ref, pos, pos + 1):
+        coverage = pileupcolumn.nsegments
+        break
+
+    return coverage
+
+
+def add_coverage(softclipdata, pileup):
+    """ The add coverage function adds the coverage to the softclipdata dictionary.
+
+    :param softclipdata: A dictionary with positions and the number of softclip bases.
+    :param pileup: Pysam object containing read information of all positions in the region.
+    :return softclipdata: A dictionary with positions with the number of softclip bases and coverage.
+    """
+    for pileupcolumn in pileup:
+        if pileupcolumn.pos in softclipdata:
+            softclipdata[pileupcolumn.pos][1] = pileupcolumn.nsegments
+
+    return softclipdata
+
+
+def to_graphdata(softclipdata):
+    """ The to_graphdata function receives the softclipdata dictionary and changes it to a 2d list datastructure that is
+    sorted on starting position and compressed by merging positions with the same ratio.
+
+    :param softclipdata: A dictionary with positions with the number of softclip bases and coverage.
+    :return compressed_graphdata: A 2d list containing the coordinates and the percentage of sofclipped bases.
+    """
+    graphdata = []
     chromosome = args.region.split(':')[0]
 
-    for pos in sofclipdata:
-        if sofclipdata[pos][1] != 0:
-            ratio = round(sofclipdata[pos][1]/(sofclipdata[pos][0]+sofclipdata[pos][1]), 2)
-            if not first:
-                if ratio == heatmapdata[-1][3] and heatmapdata[-1][2] == (pos-1):
-                    old = heatmapdata[-1]
-                    old[2] = pos
-                else:
-                    heatmapdata.append([chromosome, pos, pos, ratio])
-            else:
-                heatmapdata.append([chromosome, pos, pos, ratio])
-                first = False
+    for pos in softclipdata:
+        percentage = round(softclipdata[pos][0] / (softclipdata[pos][0] + softclipdata[pos][1]), 2)
+        graphdata.append([chromosome, pos, pos, percentage])
 
-    return heatmapdata
+    graphdata = sorted(graphdata, key=lambda x: x[1])
 
+    compressed_graphdata = [graphdata[0]]
 
-def sort_flags(flags):
-    """ The sort_flags function sorts the flags on starting position using insertionsort.
+    for row in graphdata[1:]:
+        if row[3] == compressed_graphdata[-1][3] and (row[2] + 1) == compressed_graphdata[-1][3]:
+            compressed_graphdata[-1][2] = row[2]
+        else:
+            compressed_graphdata.append(row)
 
-    :param flags: a 2d list containing all the flag information.
-    :return flags: a 2d list containing all the flag information.
-    """
-    for i in range(1, len(flags)):
-        key = flags[i][1]
-
-        j = i - 1
-        while j >= 0 and key < flags[j][1] and flags[i][0] == flags[j][0]:
-            temp = flags[j+1]
-            flags[j+1] = flags[j]
-            flags[j] = temp
-            j -= 1
-        flags[j + 1][1] = key
-
-    return flags
+    return compressed_graphdata
 
 
-def write_bedgraph_file(heatmapdata):
+def write_bedgraph_file(graphdata):
     """ The write_bedgraph_file function receives the heatmapdata and writes a BedGraph file.
 
     :param heatmapdata: A 2d list containing the coordinates and the percentage of sofclipped bases.
@@ -219,8 +192,8 @@ def write_bedgraph_file(heatmapdata):
         bedfile.write('track type=bedGraph name=Softclip_graph description="Softclip graph" color=220,20,60 '
                       'graphType=bar alwaysZero=off\n')
 
-    for datapoint in heatmapdata:
-        with open(args.output + f'/{args.name}.BedGraph', 'a') as bedfile:
+    with open(args.output + f'/{args.name}.BedGraph', 'a') as bedfile:
+        for datapoint in graphdata:
             bedfile.write(f"{datapoint[0]}\t{datapoint[1]}\t{datapoint[2]}\t{datapoint[3]}\n")
 
 
@@ -231,9 +204,9 @@ def write_logfile(read_data):
     current_time = datetime.now().strftime("%H:%M:%S")
     current_day = date.today().strftime("%d/%m/%Y")
 
-    text = f'Logfile created by: {current_path}/softclip_heatmap.py\nScript finished at: {current_time} {current_day}\n' \
-           f'{"-"*40}Read data{"-"*40}\nTotal reads: {read_data[0]}\nUnmapped reads: {read_data[1]}\n' \
-           f'Reads without matches: {read_data[2]}\n{"-"*40}Parameters{"-"*40}\nRegion: {args.region}\n' \
+    text = f'Logfile created by: {current_path}/softclip_graph.py\nScript finished at: {current_time} {current_day}\n' \
+           f'{"-" * 40}Read data{"-" * 40}\nTotal reads: {read_data[0]}\nUnmapped reads: {read_data[1]}\n' \
+           f'Reads without matches: {read_data[2]}\n{"-" * 40}Parameters{"-" * 40}\nRegion: {args.region}\n' \
            f'Bamfile: {args.bam}\nOutput_folder: {args.output}\n'
 
     with open(args.output + f'/{args.name}_BedGraph_log.txt', 'w') as logfile:
@@ -241,15 +214,15 @@ def write_logfile(read_data):
 
 
 if __name__ == '__main__':
-    reads = fetch_reads()
+    reads, pileup = fetch_reads()  # fetch reads and pileup engine
 
-    softclipdata, read_data = get_softclipdata(reads)
+    softclipdata, read_data = get_softclipdata(reads)  # retreive number of softclips and read information
 
-    graphdata = get_heatmapdata(softclipdata)
+    softclipdata = add_coverage(softclipdata, pileup)  # add coverage to softclipdata to calculate ratio
 
-    sorted_graphdata = sort_flags(graphdata)
+    graphdata = to_graphdata(softclipdata)  # change datastructure of softclipdata
+
+    write_bedgraph_file(graphdata)  # write to BedGraph formatted file
 
     if args.log:
-        write_logfile(read_data)
-
-    write_bedgraph_file(sorted_graphdata)
+        write_logfile(read_data)  # write logfile
